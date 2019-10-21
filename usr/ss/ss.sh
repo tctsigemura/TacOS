837d60cebfe9276a897ba286aa56d538a58dcc3f
tctsigemura <sigemura@tokuyama.ac.jp>
2019-10-21 11:45:01 +0900

    C--新しいバージョンで再コンパイル

--- usr/ss/ss.sh
+++ usr/ss/ss.sh
@@ -1,31 +1,31 @@
-#!/bin/bash
-# TacOS Source Code
-#    Tokuyama kousen Advanced educational Computer.
-#
-# Copyright (C) 2019 by
-#                      Dept. of Computer Science and Electronic Engineering,
-#                      Tokuyama College of Technology, JAPAN
-#
-#   上記著作権者は，Free Software Foundation によって公開されている GNU 一般公
-# 衆利用許諾契約書バージョン２に記述されている条件を満たす場合に限り，本ソース
-# コード(本ソースコードを改変したものを含む．以下同様)を使用・複製・改変・再配
-# 布することを無償で許諾する．
-#
-#   本ソースコードは＊全くの無保証＊で提供されるものである。上記著作権者および
-# 関連機関・個人は本ソースコードに関して，その適用可能性も含めて，いかなる保証
-# も行わない．また，本ソースコードの利用により直接的または間接的に生じたいかな
-# る損害に関しても，その責任を負わない．
-#
-#
-#
+#!/bin/sh
 
-# ss/ss.sh : ファイル送信プログラム
-#
-# 2019.10.15 : 新規作成
-#
-# $Id$
-#
+if [ $# -lt 1 ]; then
+  echo "Usage : `basename $0` <filename> [<as filename>]" >&2
+  exit 1
+fi
 
-stty raw
-./ss $*
-stty -raw
+case `uname` in
+#  Linux )
+#     dev="/dev/ttyUSB0" ;;
+#  FreeBSD )
+#     dev="/dev/cuaU0" ;;
+  Darwin )
+     dev=`ls /dev/cu.usbserial-* 2>/dev/null` ;;
+  * )
+     echo "`uname` is nsupported." >&2 ;;
+esac
+
+if [ `echo ${dev} | wc -w` -lt 1 ]; then
+  echo "No devices were detected." >&2
+  exit 1
+fi
+
+if [ `echo ${dev} | wc -w` -gt 1 ]; then
+  echo "Two or more devices were detected." >&2
+  echo "${dev}" | awk '{printf("\t%s\n", $0);}' >&2
+  echo "Detach needless devices." >&2
+  exit 1
+fi
+
+ss.bin $@ > ${dev} < ${dev}
